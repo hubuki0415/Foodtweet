58e6d41b069bfa404f09294322b61329c6b22605
hubuki0415 <mangan24@icloud.com>
2020-09-28 15:52:21 +0900

    ツイート投稿処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # root  'posts#index' 
   # resources :posts, only: [:index]
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new]
+  resources :tweets, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
