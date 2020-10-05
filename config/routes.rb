a7ab7a1f47219b3921f98fa0cce729afa5750714
hubuki0415 <mangan24@icloud.com>
2020-10-05 17:15:22 +0900

    ツイートにユーザー情報を追加~マイページの作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # resources :posts, only: [:index]
   root to: 'tweets#index'
   resources :tweets
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
